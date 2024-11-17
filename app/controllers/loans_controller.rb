d438924b86409cf3648eabe491061bc561418ebf
Gusthavo Alencar <56687423+gusthavoalencar@users.noreply.github.com>
2024-11-17 22:33:26 +0000

    loans functionality & UI for crud

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,11 +1,48 @@
 # frozen_string_literal: true
 
-class LoansController < ApplicationController # rubocop:todo Style/Documentation
-  def new; end
+class LoansController < ApplicationController
+  def new
+    @loan = Loan.new
+  end
 
-  def create; end
+  def create
+    @loan = Loan.new(loan_params)
+    if @loan.save
+      redirect_to @loan, notice: 'Loan was successfully created.'
+    else
+      render :new
+    end
+  end
 
-  def index; end
+  def index
+    @loans = Loan.all
+  end
 
-  def show; end
+  def show
+    @loan = Loan.find(params[:id])
+  end
+
+  def loan_params
+    params.require(:loan).permit(:amount, :interest_rate, :status, :user_id)
+  end
+
+  def edit
+    @loan = Loan.find(params[:id])
+  end
+
+  def update
+    @loan = Loan.find(params[:id])
+    if @loan.update(loan_params)
+      redirect_to @loan, notice: 'Loan was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @loan = Loan.find(params[:id])
+    @loan.destroy
+    redirect_to loans_path, notice: 'Loan was successfully deleted.'
+  end
+
 end
